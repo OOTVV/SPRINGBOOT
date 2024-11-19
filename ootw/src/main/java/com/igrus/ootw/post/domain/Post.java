94bd248bb7216eae0c48cf849d0d1d3ddce494a6
sengjun0624 <sengjun0624@naver.com>
2024-11-19 23:49:21 +0900

    [FEAT] Add Post Domain Class

--- ootw/src/main/java/com/igrus/ootw/post/domain/Post.java
+++ ootw/src/main/java/com/igrus/ootw/post/domain/Post.java
@@ -1,4 +1,39 @@
 package com.igrus.ootw.post.domain;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class Post {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String content;
+
+	private Integer likes = 0;
+
+	private Integer views = 0;
+
+	@Column(name = "created_at")
+	private LocalDateTime createdAt;
+
 }
