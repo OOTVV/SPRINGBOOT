f3f21dd7a32c007c59b039512577229168429bf2
이재준 <junny@ijaejun-ui-MacBookPro.local>
2024-11-27 21:32:25 +0900

    [FEAT] 게시물 생성

--- ootw/src/main/java/com/igrus/ootw/post/application/PostService.java
+++ ootw/src/main/java/com/igrus/ootw/post/application/PostService.java
@@ -1,4 +1,29 @@
 package com.igrus.ootw.post.application;
 
-public interface PostService {
+import com.igrus.ootw.post.domain.Post;
+import com.igrus.ootw.post.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+
+    private final PostRepository postRepository;
+
+
+    // 게시물 생성 메서드
+    public Post createPost(Post post) {
+        // 데이터베이스에 게시물 저장 후, 저장된 게시물을 반환
+        return postRepository.save(post);
+    }
+
+    public Post getPost(Long id) {
+        Optional<Post> byId = postRepository.findById(id);
+        Post createdPost = byId.orElse(null);
+        // 데이터베이스에 게시물 저장 후, 저장된 게시물을 반환
+        return createdPost;
+    }
 }
