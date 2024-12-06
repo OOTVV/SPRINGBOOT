8bb4b60965613b7b1db8bb93e74642977e731eff
sengjun0624 <sengjun0624@naver.com>
2024-12-06 17:18:55 +0900

    [FEAT] Add Relationship Mapping in Comment

--- ootw/src/main/java/com/igrus/ootw/comment/domain/Comment.java
+++ ootw/src/main/java/com/igrus/ootw/comment/domain/Comment.java
@@ -1,42 +1,43 @@
-package com.igrus.ootw.comment.entity;
+package com.igrus.ootw.comment.domain;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.*;
 import lombok.*;
 
 import com.igrus.ootw.post.domain.Post;
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Getter
+import com.igrus.ootw.user.domain.User;
 
 @Entity
-@Table(name = "comments") // 테이블 이름 comments
+@Getter
+@Setter
+@Builder
+@Table(name="comment")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class Comment {
-
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // Primary Key 자동 생성
-    private Long commentid;  // 내 댓글 키
-
-    @Column
-    private long parentIdx; // 부모 댓글의 commentIdx
-
-    @Column
-    private long orderNumber;//댓글 묶음(부모+자식)의 순서
+    private Long id;  // 내 댓글 키
 
     @Column(columnDefinition = "TEXT",nullable = false)
-    private String commentContent;  // 내용
+    private String content;  // 내용
 
-    @ManyToOne
-    @JoinColumn(name = "posts_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
     private Post post;
-    // 왜래키 (Post 클래스)
-/*
-    @ManyToOne
-    @JoinColumn(name = "ID")
-    private USER user; // 왜래키 (User 클래스 id) 작성자 ID
-//대댓글은 일단 미루고 댓글 기능부터
-    */
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_comment_id")
+    private Comment parentComment;
+
+    @OneToMany(mappedBy = "parentComment", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> childComments = new ArrayList<>();
 
 }
